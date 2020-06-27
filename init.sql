e0d4537ab0257612f2206c619ffeb53eb663cfb0
Adrian Urbański <adrianu161999@gmail.com>
2020-06-27 20:47:27 +0200

    Add indexing

--- init.sql
+++ init.sql
@@ -23,3 +23,13 @@ CREATE TABLE guests (
     stay_date DATE,
     PRIMARY KEY (cyclist, node, stay_date)
 );
+
+CREATE INDEX nodes_btree_node ON nodes USING btree(node);
+CREATE INDEX nodes_gist_location ON nodes USING GIST(location);
+
+CREATE INDEX catalog_btree_version ON trip_catalog USING btree(version);
+
+CREATE INDEX trip_btree_cyclist ON trips USING btree(cyclist);
+CREATE INDEX trip_btree_version ON trips USING btree(version);
+
+CREATE INDEX guests_btree_date_node ON guests USING btree(stay_date, node);
